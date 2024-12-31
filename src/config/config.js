6c275bff250e6af8f31fedfbd5e40e2f8840dd6c
Yuhao Zhang(Eric) <ericzhang041227@gmail.com>
2024-12-31 02:19:23 -0500

    template id changed

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const params = {
     appid: 'wx687bcfbba45fb057',
     secret: '1141d1e263343b8e0f1d5fb40a6f22fb',
     touser: 'oi1pG7Dsduv_mJhg8HcJmYpDW7uw',
-    template_id: '1bgDPOi4ZeG5pWL5gGaE59m-Ua9LxVQdux6BLVABM3o',
+    template_id: 'n76rv70cOsbXob8wRLRWNweyjAHZsGn2rVQeqCkWDt0',
 
 }
 
